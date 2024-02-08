46100f032faff7927b3dbc634e8f193330932e09
Daniel Bautista <68715227+daniel-baf@users.noreply.github.com>
2024-02-08 01:24:17 -0600

    Matrix (#1)
    * v0.1.0
    
    Add and sub operations
    
    * complex operation
    
    Add mult and uses numpy to generate inverse for divitions
    
    * remove unused data
    
    * gitignore
    
    * Matrix
    
    gen matrix from file and execute multiple operations from file

---
+++ Matrix/basic-operations/manual.py
@@ -0,0 +1,48 @@
+import numpy as np
+from Matrix.ArithSolver import Operation, ArithSolver as Solver
+from Utils.PrinterManager import PrinterManager as Printer
+
+
+def test_sub():
+    matrix_a = np.array([[1, 2, 4], [3, 0, 4], [1, 1, -4]])
+    matrix_b = np.array([[5, 6, 1], [7, 8, 8], [3, -2, 0]])
+    solver = Solver()
+    solver.solve_basic_operation(Operation.SUBTRACT, matrix_a, matrix_b)
+
+
+def test_sum():
+    # Example usage:
+    matrix_a = np.array([[1, 2, 4], [3, 0, 4], [1, 1, -4]])
+    matrix_b = np.array([[5, 6, 1], [7, 8, 8], [3, -2, 0]])
+    solver = Solver()
+    solver.solve_basic_operation(Operation.ADD, matrix_a, matrix_b)
+
+
+def test_mult():
+    matrix_a = np.array([[1, 2], [3, 0], [1, 1]])
+    matrix_b = np.array([[7, 8, 8], [3, -2, 0]])
+    solver = Solver()
+    solver.solve_basic_operation(Operation.MULTIPLY, matrix_a, matrix_b)
+
+
+def test_division():
+    # Define matrices A and B
+    matrix_a = np.array([[1, 2, 4, 1], [3, 0, 0, 4], [1, 1, -2, 6], [9, -2, -1, 8]])
+    matrix_b = np.array([[1, 2, 4, 1], [3, 3, 1, 9], [1, 1, 1, 1], [0, -11, 2, 1]])
+    solver = Solver()
+    solver.solve_basic_operation(Operation.DIVIDE, matrix_a, matrix_b)
+
+
+def main():
+    try:
+        # test_sum()
+        test_sub()
+        # test_mult()
+        # test_division()
+    except Exception as e:
+        Printer.custom_print(f'Error unrecoverable at program {e}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
